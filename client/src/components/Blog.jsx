3baaec2daa4d815856529dd791cd3f69ace0e4c2
yuto-kawahara <mashroom1384@gmail.com>
2021-12-08 23:36:00 +0900

    [Add]フロントとサーバーの通信成功

---
+++ client/src/components/Blog.jsx
@@ -0,0 +1,24 @@
+﻿import axios from "axios";
+import { useEffect, useState } from "react";
+
+
+export const Blog = () => {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/v1/posts')
+    .then((res) => {
+      const { data } = res.data;
+      console.log(res);
+      setPosts(data);
+    })
+  }, []);
+  return(
+    <div>
+      {posts.map((post) => (
+        <p key={post.id}>{post.title}</p>
+      ))}
+    </div>
+  );
+}
