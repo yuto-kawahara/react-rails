4be1cd9443bcdc0f9766982c941328da5d49273e
yuto-kawahara <mashroom1384@gmail.com>
2021-12-09 17:46:11 +0900

    [Add]React側からPost送信成功

--- client/src/components/Blog.jsx
+++ client/src/components/Blog.jsx
@@ -1,21 +1,57 @@
-﻿import axios from "axios";
+import axios from "axios";
 import { useEffect, useState } from "react";
-
+import {Button, Input} from "@mui/material";
 
 export const Blog = () => {
-
   const [posts, setPosts] = useState([]);
+  const [createpost, setCreatepost] = useState({
+    title: "",
+    content: ""
+  });
 
+  const createPost = (e) => {
+    axios.post('http://localhost:3000/api/v1/posts',{
+      title: createpost.title,
+      content: createpost.content
+    }).then(res => {
+      const { data } = res.data;
+      setPosts([...posts, {
+        id: data.id,
+        title: data.title,
+        content: data.content
+      }])
+    })
+
+  }
   useEffect(() => {
     axios.get('http://localhost:3000/api/v1/posts')
     .then((res) => {
       const { data } = res.data;
-      console.log(res);
+      console.log(data);
       setPosts(data);
     })
   }, []);
   return(
     <div>
+      <form onSubmit={createPost}>
+        <Input 
+            type="text"
+            name="post"
+            value={createpost.title}
+            placeholder="Enter text"
+            onChange={e => setCreatepost({title: e.target.value})} 
+        />
+        {/* <Input 
+            type="text"
+            name="post"
+            value={createpost.content}
+            placeholder="Enter text"
+            onChange={e => setCreatepost({content: e.target.value})} 
+        /> */}
+        <Button type="submit" variant="contained" color="primary">
+          投稿
+        </Button>
+      </form>
       {posts.map((post) => (
         <p key={post.id}>{post.title}</p>
       ))}
